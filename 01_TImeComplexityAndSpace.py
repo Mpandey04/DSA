068fff841a788c4d112cdd68fce28029fe0d1e64
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-11 17:14:48 +0530

    Time complexity and space complexity

---
+++ 01_TImeComplexityAndSpace.py
@@ -0,0 +1,34 @@
+#Time complexity:Rate of increase in time with respect to input size.
+
+#Time complexity-Big-O-Notation-O(3N)
+#1. Always calculate time complexity in worst case
+#2. Avoid the constant values/Avoid lower values
+#3.Avoid lower bound
+
+# age=int(input('Enter age'))
+# if age>=80:
+#     print("SUper senior")
+# elif (age>=60 and age<80):
+#     print("senior")
+# elif (age>=24 and age<60):
+#     print("Working")
+# elif (age>=16 and age<24):
+#     print("Teen")
+# else:
+#     print("baby")
+
+#Different types of time complexity
+# 1.Big-O(n)-> Worst case->Upper bound
+#2. Theta()->Average case
+# 3.Omega-> best case[lower bound]
+
+# for i in range(1,n+1):
+#     for j in range(i,n+1): # Time complexity-O(n2)
+#         pass
+
+# space complexity- 
+# ->memory space-> Big-O-notation
+# Auxiliary space + input space
+# Auxiliary space:- The extra space used to solve the problem.
+# Input space:- The space used to store the input.
+# Do not change the input unless asked.
