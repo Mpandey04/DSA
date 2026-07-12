efdf8adbdecffff926957c436cdce0d089f07062
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-12 16:25:11 +0530

    Time Complexity of common operator

---
+++ 03_TimeComplexityOfCommonOperator.py
@@ -0,0 +1,24 @@
+# Common operation of list and there time complexity
+#Tc-Time complexity
+
+li=[1,2,3,4,5]
+
+#n denote number of input
+# li2=li.copy() # copy take TC O(n) in average case or worst case.
+# li2=li.append(7) # append take TC O(1) in average case or worst case
+# li.pop()  # pop take TC O(1) in average case or worse case
+# li.pop[3] # pop intermediate TC in worst case O(n)
+# li.insert(10) # Insert TC O(n) in average or worst case
+# print(li[4]) # get element TC O(1) in worse case or average case
+# Set item-> TC O(1)
+# Delete item-> TC O(n)
+# Iteration-> TC O(n)
+# Get Slice-> TC O(n)
+# Get slice-> TC O(jitna part aap slice kroge agar esko assume kre uska length k hai to time complexity hoga O(k))
+# Del slice-> TC O(n)
+# set slice-> TC O(k+n)
+# Extend[1]-> TC O(k)
+# sort -> TC O(nlogn)
+# multiply-> TC O(nk)
+# x in s -> TC O(n)
+# min(s),max(s)-> 
