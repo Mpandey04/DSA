9c2676fd24b245e601bfb83085e7bacba639b9b5
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-12 16:44:18 +0530

    Palindrome number

--- 06_ReverseOfNumber.py
+++ 06_ReverseOfNumber.py
@@ -1,1 +1,15 @@
 n=int(input("Give any number for checking palindrome\n"))
+num=n
+reverse=0
+while num>0:
+    ld=num%10
+    reverse=(reverse*10)+ld
+    num=num//10
+    
+if(n==reverse):
+    print(f"{n} is a palindrome number")
+else:
+    print(f"{n} is not a palindrome number")
+    
+#Time complexity- O(log(n))
+#space complexity- O(1)
