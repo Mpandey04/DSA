4c03f566f33d9514227baa47c2d4db539bd59efa
Mpandey04 <manishkumarpanday231@gmail.com>
2026-07-17 11:03:49 +0530

    Bubble sort

---
+++ 19_BubbleSort.py
@@ -0,0 +1,21 @@
+# Bubble short is basically adjacent swap
+n=int(input("What should be your length for your array or list between 1 to 10\n"))
+li=[]
+for i in range(1,n+1):
+    num=int(input((f"Enter {i} element:")))
+    li.insert(i,num)
+    
+print(li)
+def BubbleSort(arr):
+    n=len(arr)    
+    for i in range(n-2,-1,-1):
+        for j in range(0,i+1):
+            if (arr[j]>arr[j+1]):
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    
+print("="*40)
+BubbleSort(li)
+print(li)
+
+#Time complexity=O(n(n+1)/2)
+# Space complexity=O(1)
---
+++ 20_InsertionSort.py
@@ -0,0 +1,24 @@
+num=int(input("What should be size of your list between 1 to 10:"))
+li=[]
+for i in range(0,num):
+    n=int(input(f"Enter {i} element:"))
+    li.insert(i,n)
+    
+def insertionSort(arr):
+    n=len(arr)
+    index=0
+    for i in range(1,n):
+        index=arr[i]
+        j=i-1
+        while (j>=0 and arr[j]>index):
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=index
+
+print(li)
+print("="*50)
+insertionSort(li)
+print(li)
+
+# Time Complexity-O(n(n+1)/2)~~O(N square)
+# Space complexity-
